7d32da2211272b916e12515bebb7b38228e704ac
DHAMOTHARANJ <dhamothranj@outlook.com>
2023-11-19 15:56:13 +0530

    change

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-dark text-light p-4'> 
-      <h6 className="text-center">All rights reserved &copy; karthiinfo</h6>
+      <h6 className="text-center">Happy to see you here, Thanks for visiting our page</h6>
     </div>
   )
 }
